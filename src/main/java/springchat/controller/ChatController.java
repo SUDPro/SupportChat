9d03ba5258742aa337ef8a21fee7258ea36a606e
SUDPro <32507728+SUDPro@users.noreply.github.com>
2019-07-09 10:13:13 +0300

    Update ChatController.java

--- src/main/java/springchat/controller/ChatController.java
+++ src/main/java/springchat/controller/ChatController.java
@@ -4,6 +4,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestParam;
 import springchat.model.ChatMessage;
 import org.slf4j.Logger;
+import org.springframework.http.ResponseEntity;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
