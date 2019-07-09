30d766a870dd9c7dbadaa0a6ed20680cfd12b86b
SUDPro <32507728+SUDPro@users.noreply.github.com>
2019-07-09 09:55:27 +0300

    Create Item.java

---
+++ src/main/java/springchat/model/Item.java
@@ -0,0 +1,23 @@
+package springchat.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.awt.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Item{
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private Srting status;
+  private Strig description;
+}
